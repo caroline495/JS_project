9a07b797a7ee060b68a230940b8259ffa413a4e5
Caroline Zhang <cczhang1@yahoo.com>
2024-03-11 09:16:22 -0700

    Create View class and initial methods

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
+import View from "./scripts/view";
+
 console.log("Webpack is working!")
 
+// eventlistenber for click
+let view1 = new View("1800","1899");
+let num = view1.numHighlights();
+// view1.getCountry();
+// view1.getDepartments();
+// view1.getYears();
+// console.log(num, "num is");
+// view1.getArtists();
+
+
+
+
+
+// Need to work on:
+// Search option, search by category
+// search by (title, artist, year, material, tag)
+
+
+
+// TEST CODE --------------------------------------
 // test webpack working, manipulate html element
 // document.querySelectorAll("ul.works > li").forEach( element => {
 //     element.style.color = "red";
 // });
 
 // testing The Met API link up
-// const id = "bcb83c4b54aee8418983c2aff3073b3b";
-// const city = "London,UK";
 
 // GET /public/collection/v1/objects : returns a listing of all valid Object IDs available to use
 // 487401 objects
@@ -34,118 +54,3 @@ console.log("Webpack is working!")
 //       console.log(errorResponse);
 //     }
 //   );
-
-// function to save data from fetch
-function collectData(data) {
-    return data
-}
-
-// dataset = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${dateBegin}&dateEnd=${dateEnd}&q=*`)
-//   .then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error(response);
-//     }
-//   })
-//   .then(
-//     data => {
-//       console.log("Success!");
-//       console.log(data);
-//     //   console.log(data["objectIDs"]);
-//       set = [...data["objectIDs"]];
-//       console.log("this is dataset");
-//       console.log(set, "hello");
-//       return collectData(set);
-//     },
-//     errorResponse => {
-//       console.log("Failure!");
-//       console.log(errorResponse);
-//     }
-//   );
-
-// Fetch for time period and isHighlight
-const dateBegin= "1800";
-const dateEnd = "1899";
-let dataset;
-let artists = {};
-function addToArtists(object, artist) {
-    if (!object[artist]) {
-        object[artist] = 1;
-    } else {
-        object[artist] += 1;
-    }
-    return object
-}
-
-let sum = 0;
-dataset = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${dateBegin}&dateEnd=${dateEnd}&q=*`)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(response);
-    }
-  })
-  .then(
-    data => {
-      console.log("Success!");
-      console.log(data);
-    //   console.log(data["objectIDs"]);
-      set = [...data["objectIDs"]];
-      console.log("this is dataset");
-      return collectData(set);
-    },
-    errorResponse => {
-      console.log("Failure!");
-      console.log(errorResponse);
-    })
-    .then(data => {
-        data.forEach((element, idx) => {
-            //if (idx < 10) {
-            fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`)
-            .then(response => {
-                console.log("here");
-                return response.json()
-            })
-            .then(response => {
-                // console.log("here2");
-                // console.log(response, idx);
-                // console.log(response.artistDisplayName);
-                addToArtists(artists, response.artistDisplayName);
-                 
-                if (idx === data.length - 1) {
-                    console.log(artists, "artists");
-                    Object.values(artists).forEach(ele => {
-                        sum += ele;
-                    })
-                    console.log(sum, "sum");
-                    let arr = Object.entries(artists).slice();
-                    arr.sort((a, b) => b[1] - a[1]);
-                    console.log(arr.slice(0,14));
-                }
-            });
-            //}
-        })
-        return artists;
-    })
-    .then(response => {
-        // Object.values(artists).forEach(ele => {
-        //     sum += ele;
-        // })
-        // console.log(sum, "sum");
-    });
-
-  // Search option, search by category
-  // search by (title, artist, year, material, tag)
-
-  // View for each time period:
-  // Show % highlights among total number of works 
-  //  - pie chart for this with % highlights vs. non highlights (show specific numbers in hover description)
-
-  // Show top artists (artists with the most works)
-  //  - collect all object ids that fall within a certain time period, then iterate through those 
-  // Show % of regions represented 
-  // Show makeup of Years of Accession
-  // Show top tags 
-  // - could use the search query to search tags
---
+++ src/scripts/view.js
@@ -0,0 +1,308 @@
+class View {
+    // View for each time period:
+    // Show % highlights among total number of works 
+    //  - pie chart for this with % highlights vs. non highlights (show specific numbers in hover description)
+
+    // Show top artists (artists with the most works)
+
+    // Show % of regions represented 
+    // Show makeup of Years of Accession
+
+    constructor(dateBegin, dateEnd){
+        this.dateBegin = dateBegin;
+        this.dateEnd = dateEnd;
+    }
+    
+    // Returns the number of highlights from time period
+    numHighlights(){
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${this.dateBegin}&dateEnd=${this.dateEnd}&q=*`)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+        })
+        .then(
+        data => {
+            console.log("Success!");
+            console.log(data);
+            console.log(data["total"], "num");
+            let num = data["total"];
+            return num;
+        },
+        errorResponse => {
+            console.log("Failure!");
+            console.log(errorResponse);
+        })
+    }
+
+    // Helper Method to add each value from a field of an object to an array
+    addToArray(object, field) {
+        if (!object[field]) {
+            object[field] = 1;
+        } else {
+            object[field] += 1;
+        }
+        return object
+    }
+
+    // Right now: gets the top 10ish artists who have works that are highlights in the collection, between the time period of the View
+    // NOTE - ON HIGHLIGHTS, since dataset is too large...
+    // Search by criteria
+    // returns an array of object IDs that match criteria, iterate through each object by id through another fetch & get at artist, collect that info into an array
+    getArtists() {
+        let artists = {};
+        let sum = 0;
+
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${this.dateBegin}&dateEnd=${this.dateEnd}&q=*`)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+        })
+        .then(
+        data => {
+            console.log("Success!");
+            console.log(data);
+            let set = [...data["objectIDs"]];
+            return set;
+        },
+        errorResponse => {
+            console.log("Failure!");
+            console.log(errorResponse);
+        })
+        .then(data => {
+            
+            data.forEach((element, idx) => {
+                //if (idx < 10) {
+                fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`)
+                .then(response => {
+                    console.log("here");
+                    return response.json()
+                })
+                .then(response => {
+                    // console.log("here2");
+                    // console.log(response, idx);
+                    // console.log(response.artistDisplayName);
+                    this.addToArray(artists, response.artistDisplayName);
+                    
+                    if (idx === data.length - 1) {
+                        console.log(artists, "artists");
+                        Object.values(artists).forEach(ele => {
+                            sum += ele;
+                        })
+                        console.log(sum, "sum");
+                        let arr = Object.entries(artists).slice();
+                        arr.sort((a, b) => b[1] - a[1]);
+                        console.log(arr.slice(0,14));
+                    }
+                });
+                //}
+            })
+            return artists;
+        })
+
+    }
+
+    // Gets top 10ish years of works acquired (among the highlights)
+    // probably makes sense to refactor to show by different year buckets, for data that makes more sense to read/see
+    getYears() {
+        let years = {};
+        let sum = 0;
+
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${this.dateBegin}&dateEnd=${this.dateEnd}&q=*`)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+        })
+        .then(
+        data => {
+            console.log("Success!");
+            console.log(data);
+            let set = [...data["objectIDs"]];
+            return set;
+        },
+        errorResponse => {
+            console.log("Failure!");
+            console.log(errorResponse);
+        })
+        .then(data => {
+            
+            data.forEach((element, idx) => {
+                //if (idx < 10) {
+                fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`)
+                .then(response => {
+                    console.log("here");
+                    return response.json()
+                })
+                .then(response => {
+                    this.addToArray(years, response.accessionYear);
+                    
+                    if (idx === data.length - 1) {
+                        console.log(years, "years");
+                        Object.values(years).forEach(ele => {
+                            sum += ele;
+                        })
+                        console.log(sum, "sum");
+                        let arr = Object.entries(years).slice();
+                        arr.sort((a, b) => b[1] - a[1]);
+                        console.log(arr.slice(0,14));
+                    }
+                });
+                //}
+            })
+            return years;
+        })
+
+    }
+
+    // Gets the top 14 countries 
+    // probably makes sense to refactor to show by different year buckets, for data that makes more sense to read/see
+    getCountry() {
+        let countries = {};
+        let sum = 0;
+
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${this.dateBegin}&dateEnd=${this.dateEnd}&q=*`)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+        })
+        .then(
+        data => {
+            console.log("Success!");
+            console.log(data);
+            let set = [...data["objectIDs"]];
+            return set;
+        },
+        errorResponse => {
+            console.log("Failure!");
+            console.log(errorResponse);
+        })
+        .then(data => {
+            
+            data.forEach((element, idx) => {
+                //if (idx < 10) {
+                fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`)
+                .then(response => {
+                    console.log("here");
+                    return response.json()
+                })
+                .then(response => {
+                    this.addToArray(countries, response.country);
+                    
+                    if (idx === data.length - 1) {
+                        console.log(countries, "countries");
+                        Object.values(countries).forEach(ele => {
+                            sum += ele;
+                        })
+                        console.log(sum, "sum");
+                        let arr = Object.entries(countries).slice();
+                        arr.sort((a, b) => b[1] - a[1]);
+                        console.log(arr.slice(0,14));
+                    }
+                });
+                //}
+            })
+            return countries;
+        })
+
+    }
+
+    // Gets the 14 departments distribution (among the highlights)
+    getDepartments() {
+        let departments = {};
+        let sum = 0;
+
+        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${this.dateBegin}&dateEnd=${this.dateEnd}&q=*`)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+        })
+        .then(
+        data => {
+            console.log("Success!");
+            console.log(data);
+            let set = [...data["objectIDs"]];
+            return set;
+        },
+        errorResponse => {
+            console.log("Failure!");
+            console.log(errorResponse);
+        })
+        .then(data => {
+            
+            data.forEach((element, idx) => {
+                //if (idx < 10) {
+                fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${element}`)
+                .then(response => {
+                    console.log("here");
+                    return response.json()
+                })
+                .then(response => {
+                    this.addToArray(departments, response.department);
+                    
+                    if (idx === data.length - 1) {
+                        console.log(departments, "departments");
+                        Object.values(departments).forEach(ele => {
+                            sum += ele;
+                        })
+                        console.log(sum, "sum");
+                        let arr = Object.entries(departments).slice();
+                        arr.sort((a, b) => b[1] - a[1]);
+                        console.log(arr.slice(0,14));
+                    }
+                });
+                //}
+            })
+            return departments;
+        })
+
+    }
+
+}
+
+
+
+
+
+// -----------------------------------------
+
+
+// dataset = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&dateBegin=${dateBegin}&dateEnd=${dateEnd}&q=*`)
+//   .then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error(response);
+//     }
+//   })
+//   .then(
+//     data => {
+//       console.log("Success!");
+//       console.log(data);
+//     //   console.log(data["objectIDs"]);
+//       set = [...data["objectIDs"]];
+//       console.log("this is dataset");
+//       console.log(set, "hello");
+//       return collectData(set);
+//     },
+//     errorResponse => {
+//       console.log("Failure!");
+//       console.log(errorResponse);
+//     }
+//   );
+
+
+export default View;

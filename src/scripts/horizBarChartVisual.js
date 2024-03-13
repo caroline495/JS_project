6c73ac9c2ec7a263e3fce54540ffd6df01001b56
Caroline Zhang <cczhang1@yahoo.com>
2024-03-12 23:28:51 -0700

    Add hover for pie and horiz bar charts

--- src/scripts/horizBarChartVisual.js
+++ src/scripts/horizBarChartVisual.js
@@ -7,91 +7,122 @@ const height = 275 - margin.top - margin.bottom
 const svg = d3.select("#example-chart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+  .append("g")  // g is group element
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Load and process the data
 // d3.csv("fileName.csv").then(data => {
 //   data.forEach(d => {
-//     d.total = +d.total;
+//     d.total = +d.total; // taking every total value and make it a number
 //   });
 
-  const data = [{"":"1", type: "a", total:"10"},
-                {"":"2", type: "b", total:"15"},
-                {"":"3", type: "c", total:"20"},
-                {"":"4", type: "d", total:"6"},
-                {"":"5", type: "e", total:"13"},
-                {"":"6", type: "f", total:"70"},
-                {"":"7", type: "g", total:"5"}
-  ]
+// div that appends to the body, but isn't visible
+var divTip = d3.select("#horiz-chart-group").append("div")
+     .attr("class", "tooltip")
+     .style("opacity", 0)
 
-  // Sort the data by total
-  data.sort(function (a, b) {
-    return d3.ascending(a.total, b.total);
+const data = [{"":"1", type: "a", total:"10"},
+              {"":"2", type: "b", total:"15"},
+              {"":"3", type: "c", total:"20"},
+              {"":"4", type: "d", total:"6"},
+              {"":"5", type: "e", total:"13"},
+              {"":"6", type: "f", total:"70"},
+              {"":"7", type: "g", total:"5"}
+              ]
+
+// Sort the data by total
+data.sort(function (a, b) {
+  return d3.ascending(a.total, b.total);
+});
+
+// Set the x and y scales
+const x = d3.scaleLinear()
+  .range([0, width]) // range of pixels where chart will live
+  .domain([0, d3.max(data, function (d) { return d.total; })]); // set of numbers will be from 0 to the biggest # in our dataset will look at all our totals and return the max
+
+const y = d3.scaleBand()  // use this with horiz bar chart
+  .range([height, 0])
+  .padding(0.1)
+  .domain(data.map(function (d) { return d.type; })); // domain is all the y-values
+
+// Create the x and y axes
+const xAxis = d3.axisBottom(x)
+  .ticks(5)
+  .tickSize(0); // remove ticks
+
+const yAxis = d3.axisLeft(y)
+  .tickSize(0)
+  .tickPadding(10);
+
+// Add vertical gridlines
+svg.selectAll("line.vertical-grid")
+  .data(x.ticks(5))
+  .enter()
+  .append("line")
+  .attr("class", "vertical-grid")
+  .attr("x1", function (d) { return x(d); })
+  .attr("y1", 0)
+  .attr("x2", function (d) { return x(d); })
+  .attr("y2", height)
+  .style("stroke", "gray")
+  .style("stroke-width", 0.5)
+  .style("stroke-dasharray", "3 3");
+
+// Create the bars for the chart
+svg.selectAll(".bar")
+  .data(data)
+  .enter().append("rect")
+  .attr("class", "bar")
+  .attr("id", function (d) { return d.total.toString(); } )
+  .attr("y", function (d) { return y(d.type); })
+  .attr("height", y.bandwidth())
+  .attr("x", 0)
+  .attr("width", function (d) { return x(d.total); })
+  .attr('fill', '#BF2720')
+
+// Add the x and y axes to the chart
+svg.append("g")
+  .attr("class", "x axis")
+  .style("font-size", "10px")
+  .attr("transform", "translate(0," + height + ")")  // this moves it from the top of the chart to the bottom
+  .call(xAxis)
+  .call(g => g.select(".domain").remove());
+
+svg.append("g")
+  .attr("class", "y axis")
+  .style("font-size", "8px")
+  .call(yAxis)
+  .selectAll('path')
+  .style('stroke-width', '1.75px');
+
+svg.selectAll(".y.axis .tick text")
+  .text(function (d) {
+    return d.toUpperCase();
   });
 
-  // Set the x and y scales
-  const x = d3.scaleLinear()
-    .range([0, width])
-    .domain([0, d3.max(data, function (d) { return d.total; })]);
+svg.on("mouseover", (evt) => {
+    // console.log(evt.target, "evt.target");
+    d3.select(evt.target).transition()
+     .duration('50')
+     .attr('opacity', '.85');
 
-  const y = d3.scaleBand()
-    .range([height, 0])
-    .padding(0.1)
-    .domain(data.map(function (d) { return d.type; }));
+     //Makes the new div appear on hover:
+     divTip.transition()
+           .duration(50)
+           .style("opacity", 1);
+           
+          let num = `${evt.target.id} works`;
+       divTip.html(num)
+       .style("left", (evt.pageX-25) + "px")
+       .style("top", (evt.pageY-10) + "px"); 
+  })
+  .on('mouseout', function (evt, d) {
+    d3.select(evt.target).transition()
+        .duration('50')
+        .attr('opacity', '1');
 
-  // Create the x and y axes
-  const xAxis = d3.axisBottom(x)
-    .ticks(5)
-    .tickSize(0); // remove ticks
-
-  const yAxis = d3.axisLeft(y)
-    .tickSize(0)
-    .tickPadding(10);
-
-  // Add vertical gridlines
-  svg.selectAll("line.vertical-grid")
-    .data(x.ticks(5))
-    .enter()
-    .append("line")
-    .attr("class", "vertical-grid")
-    .attr("x1", function (d) { return x(d); })
-    .attr("y1", 0)
-    .attr("x2", function (d) { return x(d); })
-    .attr("y2", height)
-    .style("stroke", "gray")
-    .style("stroke-width", 0.5)
-    .style("stroke-dasharray", "3 3");
-
-  // Create the bars for the chart
-  svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("y", function (d) { return y(d.type); })
-    .attr("height", y.bandwidth())
-    .attr("x", 0)
-    .attr("width", function (d) { return x(d.total); })
-    .attr('fill', '#BF2720')
-
-  // Add the x and y axes to the chart
-  svg.append("g")
-    .attr("class", "x axis")
-    .style("font-size", "10px")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-    .call(g => g.select(".domain").remove());
-
-  svg.append("g")
-    .attr("class", "y axis")
-    .style("font-size", "8px")
-    .call(yAxis)
-    .selectAll('path')
-    .style('stroke-width', '1.75px');
-
-  svg.selectAll(".y.axis .tick text")
-    .text(function (d) {
-      return d.toUpperCase();
-    });
-
-  //});
+        //Makes the new divTip disappear:
+        divTip.transition()
+            .duration('50')
+            .style("opacity", 0);
+  });
--- src/scripts/pieDataVisual.js
+++ src/scripts/pieDataVisual.js
@@ -16,7 +16,9 @@ const svg = d3.select("#piechart_viz")
     .attr("transform", `translate(${width/2}, ${height/2})`);
 
 // Create dummy data
-const data = {a: .33, b:.67}
+const data = [{title:"Highlight", value: 33},
+              {title: "Not Highlight", value: 67},
+              ]
 
 // set the color scale
 const color = d3.scaleOrdinal()
@@ -24,49 +26,85 @@ const color = d3.scaleOrdinal()
 
 // Compute the position of each group on the pie:
 const pie = d3.pie()
-  .value(function(d) {return d[1]})
+  .value(function(d) {return d.value })(data);
   // d is an array of the data values [[a, .33],[b, .67]]
-const data_set = pie(Object.entries(data))
+
+//  const data_set = pie(Object.entries(data))
 
 // div that appends to the body, but isn't visible
-var div = d3.select("body").append("div")
-     .attr("class", "tooltip-donut")
-     .style("opacity", 0);
+var divTip = d3.select("#pie-chart-group").append("div")
+     .attr("class", "tooltip")
+     .style("opacity", 0)
+
 
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('All')
-  .data(data_set)
-  .join('path')
+var path = svg
+  .selectAll('path')
+  .data(pie)
+  .enter()
+  .append('path')
+  //.join('path')
   .attr('d', d3.arc()
     .innerRadius(0)
     .outerRadius(radius)
   )
-  .attr('fill', function(d){ return(color(d.data[1])) })
+  .attr('fill', function(d){ return(color(d.data.title)) })
   .attr("stroke", "white")
   .style("stroke-width", "1px")
-  .style("opacity", 0.7)
-  .on('mouseover', function (d, i) {
-    d3.select(this).transition()
-         .duration('50')
-         .attr('opacity', '.85');
+  //.attr('transform', 'translate(0, 0)')
+  .on("mouseover", (evt, d) => {
+    // console.log(evt.target, "evt.target");
+    //const [mx, my] = d3.pointer(evt); 
+    d3.select(evt.target).transition()
+     .duration('50')
+     .attr('opacity', '.85');
 
-         //Makes the new div appear on hover:
-         div.transition()
-               .duration(50)
-               .style("opacity", 1);
-              let num = (Math.round((d.value / d.data.all) * 100)).toString() + '%';
-          div.html(num)
-               .style("left", (d3.event.pageX + 10) + "px")
-               .style("top", (d3.event.pageY - 15) + "px");
-     })
-  .on('mouseout', function (d, i) {
-      d3.select(this).transition()
-          .duration('50')
-          .attr('opacity', '1');
+     //Makes the new div appear on hover:
+     divTip.transition()
+           .duration(50)
+           .style("opacity", 1);
+           console.log(d, "d is");
+          let num = `${d.data.title}: ${(d.value).toString()}%`;
+       divTip.html(num)
+       .style("left", (evt.pageX-25) + "px")
+       .style("top", (evt.pageY-10) + "px"); 
 
-          //Makes the new div disappear:
-          div.transition()
-               .duration('50')
-               .style("opacity", 0);
-     });
+ })
+.on('mouseout', function (evt, d) {
+  d3.select(evt.target).transition()
+      .duration('50')
+      .attr('opacity', '1');
+
+      //Makes the new divTip disappear:
+      divTip.transition()
+           .duration('50')
+           .style("opacity", 0);
+ });
+  
+
+
+  // OLD
+//      d3.select(this).transition()
+//      .duration('50')
+//      .attr('opacity', '.85');
+
+//      //Makes the new div appear on hover:
+//      divTip.transition()
+//            .duration(50)
+//            .style("opacity", 1);
+//            console.log(d, "d is");
+//           let num = (100).toString() + '%';
+//       divTip.html(num)
+//            .style("left", (d3.event.pageX + 10) + "px")
+//            .style("top", (d3.event.pageY - 15) + "px");
+//  })
+// .on('mouseout', function (d, i) {
+//   d3.select(this).transition()
+//       .duration('50')
+//       .attr('opacity', '1');
+
+//       //Makes the new divTip disappear:
+//       divTip.transition()
+//            .duration('50')
+//            .style("opacity", 0);
+//  });
